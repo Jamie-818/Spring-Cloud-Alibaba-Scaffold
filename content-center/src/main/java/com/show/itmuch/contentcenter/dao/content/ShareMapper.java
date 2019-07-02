ef6b91571c810bf9e877a1b6758b90aa713cf45d
主打情歌 <1004108488@qq.com>
2019-07-02 22:57:39 +0800

    整合tkMybatis以及使用代码生成器生成代码

---
+++ content-center/src/main/java/com/show/itmuch/contentcenter/dao/content/ShareMapper.java
@@ -0,0 +1,7 @@
+package com.show.itmuch.contentcenter.dao.content;
+
+import com.show.itmuch.contentcenter.domain.entity.content.Share;
+import tk.mybatis.mapper.common.Mapper;
+
+public interface ShareMapper extends Mapper<Share> {
+}
